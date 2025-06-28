49b1f9670000b774c639002f16f4997815af1ee2
Isyaku Murtala Abdulhamid <52487641+Muri68@users.noreply.github.com>
2025-06-28 01:15:19 +0100

    Delete library_management/library_management/report/inventory_and_availability directory

--- library_management/library_management/report/inventory_and_availability/inventory_and_availability.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Isyaku Murtala and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Inventory and Availability"] = {
-	"filters": [
-
-	]
-};
